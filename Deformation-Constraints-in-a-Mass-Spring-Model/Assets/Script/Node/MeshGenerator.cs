1299e2a8db3c8338404146c6edfb4c2e68a5e8e7
Agenicy <agenicy@gmail.com>
2021-08-27 22:05:14 +0800

    head

---
+++ Deformation-Constraints-in-a-Mass-Spring-Model/Assets/Script/Node/MeshGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour
+{
+    [SerializeField] (int, int) int2_MeshSize;
+    [SerializeField] GameObject prefab_Node;
+
+    public static List<Node> list_node_AllNode = new List<Node>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        (int x, int y) = int2_MeshSize;
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                GameObject gobj_node = Instantiate(prefab_Node, transform);
+                gobj_node.transform.position = new Vector3(i, j, 0);
+
+                Node node = gobj_node.GetComponent<Node>();
+                node.Setup(i, j);
+                list_node_AllNode.Add(node);
+            }
+        }
+    }
+
+}
---
+++ Deformation-Constraints-in-a-Mass-Spring-Model/Assets/Script/Node/Node.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Vector2 v2_Position;
+
+    public void Setup(int x, int y)
+    {
+        v2_Position = new Vector2(x, y);
+    }
+
+}
